345024fb0086c04132b97c6078f4fcb5f59a6e02
epdlrnldudnj <kje0023@naver.com>
2025-11-25 21:06:48 +0900

    feat : Spring Security 필터체인 설정
    - csrf disable 설정 ( 세션/쿠키 사용하지 않음 )
    - 세션 사용 안함 설정
    - 특정 url 인증 없어도 통과
    - jwt filter ( 커스텀 필터 ) 추가

---
+++ ray_spring/spring-practice/src/main/java/com/example/spring_practice/global/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.spring_practice.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                // 세션 사용 안함
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                 )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/login",
+                                "/auth/signup",
+                                "/users/email/duplicate-check",
+                                "/users/nickname/duplicate-check",
+                                "/images/**",
+                                "/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
