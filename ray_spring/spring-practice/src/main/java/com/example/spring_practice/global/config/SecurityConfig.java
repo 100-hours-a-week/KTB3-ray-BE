eedda17af6c97f2bd41d5d02d19e33baf4edc344
epdlrnldudnj <kje0023@naver.com>
2025-11-26 15:22:35 +0900

    feat : AuthenticationManger Bean

--- ray_spring/spring-practice/src/main/java/com/example/spring_practice/global/config/SecurityConfig.java
+++ ray_spring/spring-practice/src/main/java/com/example/spring_practice/global/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.spring_practice.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -45,4 +47,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager( AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
