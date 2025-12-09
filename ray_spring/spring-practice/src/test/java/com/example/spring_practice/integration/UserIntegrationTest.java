ef48999385de15615132ebd31b08714186a18246
epdlrnldudnj <kje0023@naver.com>
2025-12-09 18:14:57 +0900

    test : UserIntegrationTest 회원가입 실패 케이스 추가

--- ray_spring/spring-practice/src/test/java/com/example/spring_practice/integration/UserIntegrationTest.java
+++ ray_spring/spring-practice/src/test/java/com/example/spring_practice/integration/UserIntegrationTest.java
@@ -9,11 +9,13 @@ import com.example.spring_practice.domain.member.service.AuthService;
 import com.example.spring_practice.global.security.JwtUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -46,11 +48,9 @@ public class UserIntegrationTest {
     @Autowired
     private JwtUtil jwtUtil;
     @Autowired
-    private AuthController authController;
-    @Autowired
-    private AuthService authService;
-    @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private EntityManager em;
 
     @BeforeEach
     public void setup() {
@@ -106,6 +106,33 @@ public class UserIntegrationTest {
         assertThat(member.isPresent()).isTrue();
         assertThat(member.get().getNickname()).isEqualTo(nickname);
         assertThat(member.get().getProfileImgUrl()).isNotNull();
+    }
+
+    @Test
+    void 회원가입_중복된_이메일_실패_409() throws Exception {
+        // given
+        String email = "signup@test.com";
+        String password = "Asdf1234@";
+        String nickname = "test";
+        // when then
+        mockMvc.perform(multipart("/auth/signup")
+                        .param("email", email)
+                        .param("password", password)
+                        .param("nickname", nickname))
+                .andExpect(status().isConflict());
+    }
+    @Test
+    void 회원가입_중복된_닉네임_실패_409() throws Exception {
+        // given
+        String email = "test@test.com";
+        String password = "Asdf1234@";
+        String nickname = "new";
+        // when then
+        mockMvc.perform(multipart("/auth/signup")
+                        .param("email", email)
+                        .param("password", password)
+                        .param("nickname", nickname))
+                .andExpect(status().isConflict());
     }
 
     @Test
@@ -171,6 +198,67 @@ public class UserIntegrationTest {
                 .andExpect(status().isOk());
 
         assertThat(passwordEncoder.matches(newPassword, memberRepository.findByEmail("test@test.com").get().getPassword())).isTrue();
+    }
+
+    @Test
+    @WithMockUser("test@test.com")
+    void 프로필_수정_이미지_성공_201() throws Exception {
+        // given
+        MockMultipartFile newProfileImage = new MockMultipartFile(
+                "profileImage",
+                "newProfile.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                "image content".getBytes()
+        );
+
+        // when // then
+        mockMvc.perform(multipart(HttpMethod.PATCH, "/users/profile")
+                        .file(newProfileImage))
+                .andExpect(status().isOk());
+
+        Optional<Member> member = memberRepository.findByEmail("test@test.com");
+        assertThat(member.isPresent()).isTrue();
+        assertThat(member.get().getProfileImgUrl()).isNotEqualTo("testUrl");
+        assertThat(member.get().getNickname()).isEqualTo("test");
+    }
+    @Test
+    @WithMockUser("test@test.com")
+    void 프로필_수정_닉네임_성공_201() throws Exception {
+        // given
+
+        // when // then
+        mockMvc.perform(multipart(HttpMethod.PATCH, "/users/profile")
+                        .param("nickname", "newNickname"))
+                .andExpect(status().isOk());
+
+        Optional<Member> member = memberRepository.findByEmail("test@test.com");
+        assertThat(member.get().getProfileImgUrl()).isEqualTo("testUrl");
+        assertThat(member.get().getNickname()).isEqualTo("newNickname");
+    }
+    @Test
+    @WithMockUser("test@test.com")
+    void 프로필_수정_이미지_닉네임_성공_201() throws Exception {
+        // given
+        MockMultipartFile newProfileImage = new MockMultipartFile(
+                "profileImage",
+                "newProfile.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                "image content".getBytes()
+        );
+
+        // when // then
+        mockMvc.perform(multipart(HttpMethod.PATCH, "/users/profile")
+                        .file(newProfileImage)
+                        .param("nickname", "newNickname"))
+                .andExpect(status().isOk());
+
+        em.flush();
+        em.clear();
+
+        Optional<Member> member = memberRepository.findByEmail("test@test.com");
+        assertThat(member.isPresent()).isTrue();
+        assertThat(member.get().getProfileImgUrl()).isNotEqualTo("testUrl");
+        assertThat(member.get().getNickname()).isEqualTo("newNickname");
     }
 
     @Test
